055c911db1993cd5279703c38751a1cdf6daaf39
Jacob Krag <j.n.krag@gmail.com>
2022-06-09 20:22:04 +0200

    layout

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,11 @@
 import styles from '../styles/layout.module.css'
+import Navbar from './navbar.js'
+
 
 function Layout({children}) {
     return ( 
         <div className={styles.container}>
+            <Navbar/>
             {children}
         </div>
      );
